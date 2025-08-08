9c81f06989b162e77763205aa2ac8901b58ede6e
BrajamohanDas-afk <Brajamohandas0390@gmail.com>
2025-08-08 20:53:52 +0530

    deleted lambda

--- Backend/lambda.js
+++
@@ -1,4 +0,0 @@
-const serverlessExpress = require('@vendia/serverless-express');
-const app = require('./server'); // This imports your existing server.js
-
-exports.handler = serverlessExpress({ app });
